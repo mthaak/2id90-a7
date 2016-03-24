4fded35ca7032f7d584a8519aeac9a62e0f2f089
Pieter van der Heijden <Pieter@Pieters-MacBook-Pro.local>
2016-03-24 08:54:36 +0100

    Added training set from peach

--- spellchecker/src/SpellChecker.java
+++ spellchecker/src/SpellChecker.java
@@ -19,7 +19,8 @@ public class SpellChecker {
             if (inPeach) {
                 peachTest(sc);
             } else {
-                nonPeachTest(sc);
+                //nonPeachTest(sc);
+                peachTraining(sc);
             }
         } catch (Exception ex) {
             System.out.println(ex);
@@ -49,6 +50,95 @@ public class SpellChecker {
                 System.out.println("Answer: " +result);
                 System.out.println();
             }
+    }
+    
+    static void peachTraining(SpellCorrector sc) throws IOException { 
+            String[] sentences = {
+                "this assay allowed us to measure a wide variety of conitions",
+                "at the hme locations there were traces of water",
+                "this assay allowed us to meassure a wide variety of conditions",
+                "this assay allowed us to measure a wide vareity of conditions",
+                "at the hoome locations there were traces of water",
+                "at the home locasions there were traces of water",
+                "the development of diabetes is present in moce that carry a transgen",
+                "the development of idabetes is present in mice that carry a transgen",
+                "the development of diabetes us present in mice that harry a transgene",
+                "boxing glowes shield the knockles not the head",
+                "boxing loves shield the knuckles nots the head",
+                "boing gloves shield the knuckles nut the head",
+                "she still refers to me has a friend but i fel i am treated quite badly",
+                "she still refers to me as a friendd but i feel i am traeted quite badly",
+                "she still refers too me as a friend but i feel i am treated quite batly",
+                "she still refers to me as a fiend but i feel i am treated quite badly",
+                "a repsonse may be any measurable biological parameter that is correlated witth the toxicant",
+                "a response may be any measurable biological prameter that is corelated with the toxicant",
+                "a respunse may be any measurable biologecal parameter that is correlated with the toxicant",
+                "a responses may be any measurable biological parametre that is correlated with the toxicant",
+                "esentially there has been no change in japan",
+                "essentially there has been no change in japan",
+                "essentially here has bien no change in japan",
+                "this advise is taking into consideration the fact that the govenrment bans political parties",
+                "this advices is taking into consideration the fact that the government bans political parties",
+                "this addvice is taking into consideration the fact that the goverment bans political parties",
+                "ancient china was one of the longst lasting societies iin the history of the world",
+                "ancient china was one of the longest lasting sosieties in the history of the wor",
+                "anicent china was one of the longest lasting societties in the history of the world",
+                "ancient china wqs one of the longest lasting societies in the histori of the world",
+                "laying in the national footbal league was my dream",
+                "playing in the national football laegue was my draem",
+                "playing in the national fotball league was my dream"
+            };
+            
+            String[] correctSentences = {
+                "Answer: this assay allowed us to measure a wide variety of conditions",
+                "Answer: at the home locations there were traces of water",
+                "Answer: this assay allowed us to measure a wide variety of conditions",
+                "Answer: this assay allowed us to measure a wide variety of conditions",
+                "Answer: at the home locations there were traces of water",
+                "Answer: at the home locations there were traces of water",
+                "Answer: the development of diabetes is present in mice that carry a transgene",
+                "Answer: the development of diabetes is present in mice that carry a transgene",
+                "Answer: the development of diabetes is present in mice that carry a transgene",
+                "Answer: boxing gloves shield the knuckles not the head",
+                "Answer: boxing gloves shield the knuckles not the head",
+                "Answer: boxing gloves shield the knuckles not the head",
+                "Answer: she still refers to me as a friend but i feel i am treated quite badly",
+                "Answer: she still refers to me as a friend but i feel i am treated quite badly",
+                "Answer: she still refers to me as a friend but i feel i am treated quite badly",
+                "Answer: she still refers to me as a friend but i feel i am treated quite badly",
+                "Answer: a response may be any measurable biological parameter that is correlated with the toxicant",
+                "Answer: a response may be any measurable biological parameter that is correlated with the toxicant",
+                "Answer: a response may be any measurable biological parameter that is correlated with the toxicant",
+                "Answer: a response may be any measurable biological parameter that is correlated with the toxicant",
+                "Answer: essentially there has been no change in japan",
+                "Answer: essentially there has been no change in japan",
+                "Answer: essentially there has been no change in japan",
+                "Answer: this advice is taking into consideration the fact that the government bans political parties",
+                "Answer: this advice is taking into consideration the fact that the government bans political parties",
+                "Answer: this advice is taking into consideration the fact that the government bans political parties",
+                "Answer: ancient china was one of the longest lasting societies in the history of the world",
+                "Answer: ancient china was one of the longest lasting societies in the history of the world",
+                "Answer: ancient china was one of the longest lasting societies in the history of the world",
+                "Answer: ancient china was one of the longest lasting societies in the history of the world",
+                "Answer: playing in the national football league was my dream",
+                "Answer: playing in the national football league was my dream",
+                "Answer: playing in the national football league was my dream"
+            };
+            
+            System.out.println(sentences.length + " " + correctSentences.length);
+            
+            int correctPhrases = 0;
+            int totalPhrases = 0;
+            for (int i = 0; i < sentences.length; i ++) {
+                String result = sc.correctPhrase(sentences[i]);
+                String correctResult = correctSentences[i];
+                if (result.equals(correctResult)) {
+                    correctPhrases ++;
+                }
+                totalPhrases ++;
+            }
+            
+            System.out.println("Score: " + correctPhrases + "/" + totalPhrases);
     }
     
     static void peachTest(SpellCorrector sc) throws IOException {
